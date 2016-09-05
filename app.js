fc0b661107aa0f31d8e4d7a471cb5ff4bf4c2b0f
lukikino <lukikino@gmail.com>
2016-09-05 13:59:52 +0800

    configl

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(express.static('assets'));
+app.use("/assets", express.static('assets'));
 
 /*
  * Be sure to setup your config values before running this code. You can 
